9bb598f522d33c0fbd4a5437fecf8c3d8dd84fe6
Patrick Pedersen <ctx.xda@gmail.com>
2018-05-01 14:50:17 +0200

    Installer: Update copyright year
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- Ubuntu-IBM-Notes-9.0.1.sh
+++ Ubuntu-IBM-Notes-9.0.1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2016 Patrick Pedersen <ctx.xda@gmail.com>
+# Copyright 2018 Patrick Pedersen <ctx.xda@gmail.com>
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
